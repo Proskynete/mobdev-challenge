203178adb0565f6fdb2e83243409354ddea62d5f
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2024-01-14 23:06:15 -0300

    chore: install and config tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./assets/styles/app-base.css";
+import "./assets/styles/app-components.css";
+import "./assets/styles/app-utilities.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import { App } from "./app.tsx";
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root!).render(<App />);

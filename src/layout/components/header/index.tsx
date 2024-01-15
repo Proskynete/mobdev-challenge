261c790be67636da0ee6614faac1791e8a15c517
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2024-01-15 06:35:21 -0300

    style: add header padding

--- src/layout/components/header/index.tsx
+++ src/layout/components/header/index.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-gray-800 px-6">
+    <div className="flex flex-col justify-center items-center bg-gray-800 px-6 py-4">
       <h1 className="text-2xl font-bold text-white">mobdev challenge!</h1>
       <p className="text-center text-white">
         Encuentra imágenes (en forma aleatoria) de perros de raza.
